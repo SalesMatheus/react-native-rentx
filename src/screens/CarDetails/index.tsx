465310054d264de6074a8897c01ffe0de2525b5c
Matheus Sales <matheus.carvalho.sales@gmail.com>
2021-10-31 11:06:17 -0300

    Exibindo icones dinamicamente

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -7,12 +7,7 @@ import { Accessory } from '../../components/Accessory';
 import { Button } from '../../components/Button';
 import { CarDTO } from '../../dtos/CarDTO';
 
-import SpeedSvg from '../../assets/speed.svg';
-import AccelerationSvg from '../../assets/acceleration.svg';
-import ForceSvg from '../../assets/force.svg';
-import GasolineSvg from '../../assets/gasoline.svg';
-import ExchangeSvg from '../../assets/exchange.svg';
-import PeopleSvg from '../../assets/people.svg';
+import { getAccessoryIcon } from '../../utils/getAccessoryIcon';
 
 import {
   Container,
@@ -77,7 +72,7 @@ export function CarDetails() {
                             <Accessory
                                 key={accessory.type} 
                                 name={accessory.name} 
-                                icon={SpeedSvg} 
+                                icon={getAccessoryIcon(accessory.type)} 
                             />
                         ))
 
