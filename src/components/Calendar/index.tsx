3d2a218919151e74c1aa9caadd0de1c114ca0e18
Matheus Sales <matheus.carvalho.sales@gmail.com>
2021-11-01 08:06:18 -0300

    funções de calendario

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -2,18 +2,41 @@ import React from 'react';
 import { Feather } from '@expo/vector-icons';
 import { useTheme } from 'styled-components';
 
-import { Calendar as CustomCalendar, LocaleConfig } from 'react-native-calendars';
+import { ptBR } from './localeConfig';
+import { generateInterval } from './generateInterval';
 
-LocaleConfig.locales['pt-br'] = {
-    monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
-    monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
-    dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
-    dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
-    today: 'Hoje'
-}
+import { 
+    Calendar as CustomCalendar, 
+    LocaleConfig,
+    DateCallbackHandler, 
+} from 'react-native-calendars';
+
+LocaleConfig.locales['pt-br'] = ptBR;
 LocaleConfig.defaultLocale = 'pt-br';
 
-export function Calendar() {
+interface MarkedDateProps {
+    [date: string]: {
+        color: string;
+        textColor: string;
+        disabled?: boolean;
+        disabledTouchEvent?: boolean;
+    },
+}
+
+interface DayProps {
+    dateString: string;
+    day: number;
+    month: number;
+    timestamp: number;
+    year: number;
+}
+
+interface CalendarProps {
+    markedDates: MarkedDateProps;
+    onDayPress: DateCallbackHandler;
+}
+
+function Calendar({ markedDates, onDayPress }: CalendarProps) {
     const theme = useTheme();
 
     return (
@@ -48,6 +71,16 @@ export function Calendar() {
 
             firstDay={1}
             minDate={ new Date()}
+            markingType="period"
+            markedDates={markedDates}
+            onDayPress={onDayPress}
         />
     );
+}
+
+export {
+    Calendar,
+    MarkedDateProps,
+    DayProps,
+    generateInterval,
 }
