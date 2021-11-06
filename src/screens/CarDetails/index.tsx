6db979ef2df7ea1e6c4fe5d80af61aec2e48ff1b
Matheus Sales <matheus.carvalho.sales@gmail.com>
2021-11-06 17:31:16 -0300

    efeito de animação

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { StatusBar, StyleSheet } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { useTheme } from 'styled-components';
+
+import Animated, {
+    useSharedValue,
+    useAnimatedScrollHandler,
+    useAnimatedStyle,
+    interpolate,
+    Extrapolate
+} from 'react-native-reanimated';
 
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
@@ -12,11 +23,10 @@ import { getAccessoryIcon } from '../../utils/getAccessoryIcon';
 import {
   Container,
   Header,
-  CarImage,
-  Content,
   Details,
   Description,
   Brand,
+  CarImage,
   Name,
   Rent,
   Period,
@@ -35,6 +45,35 @@ export function CarDetails() {
     const route = useRoute();
     const { car } = route.params as Params;
 
+    const theme = useTheme();
+
+    const scrollY = useSharedValue(0);
+    const scrollHandler = useAnimatedScrollHandler(event => {
+        scrollY.value = event.contentOffset.y;
+    });
+
+    const headerStyleAnimation = useAnimatedStyle(() => {
+        return {
+            height: interpolate(
+                scrollY.value,
+                [0, 200],
+                [200, 70],
+                Extrapolate.CLAMP
+            )
+        }
+    })
+
+    const sliderCarsStyleAnimation = useAnimatedStyle(() => {
+        return {
+            opacity: interpolate(
+                scrollY.value,
+                [0, 150],
+                [1, 0],
+                Extrapolate.CLAMP
+            )
+        }
+    })
+
     function handleConfirmRental() {
         navigation.navigate('Scheduling', { car });
     }
@@ -45,15 +84,38 @@ export function CarDetails() {
 
     return (
         <Container>
-            <Header>
-                <BackButton onPress={handleBack}/>
-            </Header>
+            <StatusBar 
+                barStyle="dark-content"
+                translucent
+                backgroundColor="transparent"
+            />
+            <Animated.View 
+                style={[
+                    headerStyleAnimation, 
+                    styles.header,
+                    { backgroundColor: theme.colors.background_secondary}
+                ]}
+            >
+                <Header>
+                    <BackButton onPress={handleBack}/>
+                </Header>
 
-            <CarImage>
-                <ImageSlider imagesUrl={car.photos}/>
-            </CarImage>
-
-            <Content>
+                <Animated.View style={[sliderCarsStyleAnimation]}>
+                    <CarImage>
+                        <ImageSlider imagesUrl={car.photos}/>
+                    </CarImage>
+                </Animated.View>
+            </Animated.View>
+            <Animated.ScrollView
+                contentContainerStyle={{
+                    paddingHorizontal: 24,
+                    paddingTop: getStatusBarHeight() + 160,
+                    alignItems: 'center'
+                }}
+                showsVerticalScrollIndicator={false}
+                onScroll={scrollHandler}
+                scrollEventThrottle={16}
+            >
                 <Details>
                     <Description>
                         <Brand>{car.brand}</Brand>
@@ -79,8 +141,14 @@ export function CarDetails() {
                     }
                 </Accessories>
                 
-                <About>{car.about}</About>
-            </Content>
+                <About>
+                    {car.about}
+                    {car.about}
+                    {car.about}
+                    {car.about}
+                    {car.about}
+                </About>
+            </Animated.ScrollView>
 
             <Footer>
                 <Button title="Escolher período do aluguel" onPress={handleConfirmRental}/>
@@ -88,3 +156,11 @@ export function CarDetails() {
         </Container>
     );
 }
+
+const styles = StyleSheet.create({
+    header: {
+        position: 'absolute',
+        overflow: 'hidden',
+        zIndex: 1,
+    }
+})
