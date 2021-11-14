de287c8e1d166c609e16552c2069249cd647407f
Matheus Sales <matheus.carvalho.sales@gmail.com>
2021-11-14 16:26:42 -0300

    feature: valida formulário de login

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -3,9 +3,11 @@ import {
     StatusBar,
     KeyboardAvoidingView,
     TouchableWithoutFeedback,
-    Keyboard
+    Keyboard,
+    Alert
 } from 'react-native';
 import { useTheme } from 'styled-components';
+import * as Yup from 'yup';
 
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
@@ -21,67 +23,92 @@ import {
 } from './styles';
 
 export function SignIn() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-  const theme = useTheme();
-  return (
-      <KeyboardAvoidingView behavior="position" enabled>
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>    
-            <Container>
-                <StatusBar 
-                    barStyle="dark-content"
-                    backgroundColor="transparent"
-                    translucent
-                />
-                <Header>
-                    <Title>
-                        Estamos {'\n'} quase lá.
-                    </Title>
-                    <SubTitle>
-                        Faça seu login para começar {'\n'}
-                        uma experiência incrível.
-                    </SubTitle>
-                </Header>
+    const theme = useTheme();
 
-                <Form>
-                    <Input 
-                        iconName="mail"
-                        placeholder="E-mail"
-                        keyboardType="email-address"
-                        autoCorrect={false}
-                        autoCapitalize="none"
-                        onChangeText={setEmail}
-                        value={email}
-                    />
-                    <PasswordInput 
-                        iconName="lock"
-                        placeholder="Senha"
-                        autoCorrect={false}
-                        autoCapitalize="none"
-                        onChangeText={setPassword}
-                        value={password}
-                    />
-                </Form>
+    async function handlerSignIn(){
+        try {
+            const schema = Yup.object().shape({
+                email: Yup.string()
+                    .required('E-mail é obrigatório')
+                    .email('Digite um e-mail válido'),
+        
+                password: Yup.string()
+                    .required('Senha é obrigatória')
+            });
+        
+            await schema.validate({ email, password });
+        } catch (error) {
+            if(error instanceof Yup.ValidationError){
+                Alert.alert('Opa', error.message);
+            }else{
+                Alert.alert(
+                    'Erro na autenticação', 
+                    'Ocorreu um erro ao fazer login, verifique suas credenciais.'
+                );
+            }
+        }
+    }
 
-                <Footer>
-                    <Button 
-                        title="Login"
-                        onPress={() => {}}
-                        enabled={false}
-                        loading={false}
+    return (
+        <KeyboardAvoidingView behavior="position" enabled>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>    
+                <Container>
+                    <StatusBar 
+                        barStyle="dark-content"
+                        backgroundColor="transparent"
+                        translucent
                     />
-                    <Button 
-                        title="Criar conta gratuita"
-                        color={theme.colors.background_secondary}
-                        light={true}
-                        onPress={() => {}}
-                        enabled={true}
-                        loading={false}
-                    />
-                </Footer>
-            </Container>
-        </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
-  );
+                    <Header>
+                        <Title>
+                            Estamos {'\n'} quase lá.
+                        </Title>
+                        <SubTitle>
+                            Faça seu login para começar {'\n'}
+                            uma experiência incrível.
+                        </SubTitle>
+                    </Header>
+
+                    <Form>
+                        <Input 
+                            iconName="mail"
+                            placeholder="E-mail"
+                            keyboardType="email-address"
+                            autoCorrect={false}
+                            autoCapitalize="none"
+                            onChangeText={setEmail}
+                            value={email}
+                        />
+                        <PasswordInput 
+                            iconName="lock"
+                            placeholder="Senha"
+                            autoCorrect={false}
+                            autoCapitalize="none"
+                            onChangeText={setPassword}
+                            value={password}
+                        />
+                    </Form>
+
+                    <Footer>
+                        <Button 
+                            title="Login"
+                            onPress={handlerSignIn}
+                            enabled={true}
+                            loading={false}
+                        />
+                        <Button 
+                            title="Criar conta gratuita"
+                            color={theme.colors.background_secondary}
+                            light={true}
+                            onPress={() => {}}
+                            enabled={true}
+                            loading={false}
+                        />
+                    </Footer>
+                </Container>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
+    );
 }
